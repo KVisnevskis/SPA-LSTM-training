e1c29757c0dd3029ef628b7cf8f9d356ca054985
KVisnevskis <krisjanis.visnevskis@gmail.com>
2026-02-25 11:22:08 +0000

    moved unused const values to untracked legacy dir

--- src/spa_lstm/data/constants.py
+++ src/spa_lstm/data/constants.py
@@ -2,16 +2,6 @@
 
 from __future__ import annotations
 
-from spa_lstm.config import ColumnBounds
-
-THESIS_FIXED_BOUNDS: dict[str, ColumnBounds] = {
-    "acc_x": ColumnBounds(-12.0, 12.0),
-    "acc_y": ColumnBounds(-12.0, 12.0),
-    "acc_z": ColumnBounds(-12.0, 12.0),
-    "pressure": ColumnBounds(400.0, 1800.0),
-    "phi": ColumnBounds(-200.0, 50.0),
-}
-
 REQUIRED_BASE_COLUMNS: tuple[str, ...] = (
     "pressure",
     "acc_x",
@@ -19,4 +9,3 @@ REQUIRED_BASE_COLUMNS: tuple[str, ...] = (
     "acc_z",
     "phi",
 )
-
